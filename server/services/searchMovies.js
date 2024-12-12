c09cdb8487c0071a162368e165e400ee47a24e8f
manosatpathy <manosatpathy@gmail.com>
2024-12-12 12:16:44 +0530

    feat: complete searchMovies service function

--- server/services/searchMovies.js
+++ server/services/searchMovies.js
@@ -3,11 +3,31 @@ require("dotenv").config();
 
 const searchMovies = async (query) => {
   try {
-    const response = await axios.get(
+    const movieDetails = await axios.get(
       `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${process.env.API_KEY}`
     );
-    const result = response.data;
-    return { result, message: "working" };
+    const movies = movieDetails.data.results;
+
+    const result = await Promise.all(
+      movies.map(async (movie) => {
+        const actorsDetails = await axios.get(
+          `https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${process.env.API_KEY}`
+        );
+        const actorNames = actorsDetails.data.cast
+          .filter((actor) => actor.known_for_department === "Acting")
+          .map((actor) => actor.name);
+        return {
+          title: movie.title,
+          tmdbId: movie.id,
+          genre: movie.genre_ids,
+          actors: actorNames,
+          releaseYear: movie.release_date.slice(0, 4),
+          rating: movie.vote_average,
+          description: movie.overview,
+        };
+      })
+    );
+    return { movies: result };
   } catch (err) {
     throw err;
   }
