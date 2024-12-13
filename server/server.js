eb01dfed448a7246928ee2f9904d80dbd8e2d674
manosatpathy <manosatpathy@gmail.com>
2024-12-13 16:29:57 +0530

    chore: add database connection check

--- server/server.js
+++ server/server.js
@@ -3,9 +3,19 @@ const app = express();
 app.use(express.json());
 require("dotenv").config();
 const PORT = process.env.PORT;
-const moviesRoutes = require("./routes/movieRoute");
+const { sequelize } = require("./models");
+const movieRoutes = require("./routes/movieRoute");
+const curatedListRoutes = require("./routes/cureatedListRoute");
 
-app.use("/api/movies", moviesRoutes);
+app.use("/api/movies", movieRoutes);
+app.use("/api/curated-lists", curatedListRoutes);
+
+sequelize
+  .authenticate()
+  .then(() => console.log("database connected"))
+  .catch((error) => {
+    console.error("Unable to connect to database", error);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
