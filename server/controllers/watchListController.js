7a8a54cff2eaba53732c6be1e182a07d670f61c3
manosatpathy <manosatpathy@gmail.com>
2024-12-16 22:23:31 +0530

    feat: add watchList controller

---
+++ server/controllers/watchListController.js
@@ -0,0 +1,40 @@
+const { WatchList } = require("../models");
+const doesMovieExist = require("../services/doesMovieExists");
+const fetchMovieAndCast = require("../services/fetchMovieAndCast");
+
+const saveToWatchList = async (req, res) => {
+  const movieId = req.body.movieId;
+  try {
+    if (!movieId) {
+      return res.status(400).json({ message: "movie id is required" });
+    }
+
+    const movieInWatchList = await WatchList.findOne({ where: { movieId } });
+    if (movieInWatchList) {
+      return res
+        .status(400)
+        .json({ message: "Movie is already in your watchlist." });
+    }
+
+    const movie = await doesMovieExist(movieId);
+    if (!movie.valid) {
+      const savedMovie = await fetchMovieAndCast(movieId);
+      if (!savedMovie.valid) {
+        throw new Error("Error saving movie data into database");
+      }
+      await WatchList.create({ movieId: savedMovie.id });
+    } else {
+      await WatchList.create({ movieId: movie.id });
+    }
+
+    return res
+      .status(201)
+      .json({ message: "Movie added to watchlist successfully." });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Error adding movie to watchlist", error: err.message });
+  }
+};
+
+module.exports = saveToWatchList;
