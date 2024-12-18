b0d226451819b97bc439b412febc839f5410aa79
manosatpathy <manosatpathy@gmail.com>
2024-12-18 09:34:57 +0530

    feat: add routes for wishlist, watchlist, and review

--- server/server.js
+++ server/server.js
@@ -6,9 +6,15 @@ const PORT = process.env.PORT;
 const { sequelize } = require("./models");
 const movieRoutes = require("./routes/movieRoute");
 const curatedListRoutes = require("./routes/curatedListRoute");
+const wishListRoute = require("./routes/wishListRoute");
+const watchListRoute = require("./routes/watchListRoute");
+const reviewRoute = require("./routes/reviewRoute");
 
 app.use("/api/movies", movieRoutes);
-app.use("/api/curated-lists", curatedListRoutes);
+app.use("/api", curatedListRoutes);
+app.use("/api/movies", wishListRoute);
+app.use("/api/movies", watchListRoute);
+app.use("/api/movies", reviewRoute);
 
 sequelize
   .authenticate()
