f9101204e21914ba25af11369c429875d0c29167
manosatpathy <manosatpathy@gmail.com>
2024-12-18 09:10:01 +0530

    feat: add route to curatedlist

--- server/routes/curatedListRoute.js
+++ server/routes/curatedListRoute.js
@@ -2,10 +2,12 @@ const {
   createCuratedList,
   updateCuratedList,
 } = require("../controllers/curatedListController");
+const saveToCuratedList = require("../controllers/curatedListItemController");
 const express = require("express");
 const router = express.Router();
 
-router.post("/", createCuratedList);
-router.put("/:curatedListId", updateCuratedList);
+router.post("/curated-lists", createCuratedList);
+router.put("/curated-lists/:curatedListId", updateCuratedList);
+router.post("/movies/curated-list",saveToCuratedList);
 
 module.exports = router;
