137631716d35d1bd22335eb281e3288f405cca98
manosatpathy <manosatpathy@gmail.com>
2024-12-21 23:27:21 +0530

    feat: add GET route for searchByGenreAndActor in movieRoute

--- server/routes/movieRoute.js
+++ server/routes/movieRoute.js
@@ -1,7 +1,11 @@
-const getMovies = require("../controllers/movieController");
+const {
+  getMovies,
+  searchMoviesByGenreAndActor,
+} = require("../controllers/movieController");
 const express = require("express");
 const router = express.Router();
 
 router.get("/search", getMovies);
+router.get("/searchByGenreAndActor", searchMoviesByGenreAndActor);
 
 module.exports = router;
