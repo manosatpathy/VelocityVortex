0851dd299f71a663f30d4e270507ff7ef9403804
manosatpathy <manosatpathy@gmail.com>
2024-12-22 23:43:21 +0530

    feat: add sortByRatingOrReleaseYear function in movie controller

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,4 +1,10 @@
-const { Movie, Watchlist, Wishlist, CuratedListItem } = require("../models");
+const {
+  Movie,
+  Watchlist,
+  Wishlist,
+  CuratedListItem,
+  CuratedList,
+} = require("../models");
 const fetchMoviesByQuery = require("../services/fetchMoviesByQuery");
 
 const getMovies = async (req, res) => {
@@ -82,4 +88,71 @@ const searchMoviesByGenreAndActor = async (req, res) => {
   }
 };
 
-module.exports = { getMovies, searchMoviesByGenreAndActor };
+const sortByRatingOrYearOfRelease = async (req, res) => {
+  const { list, sortBy, order } = req.query;
+  const sortField = ["rating", "yearOfRelease"];
+  const allowedLists = ["wishList", "watchlist", "curatedList"];
+  try {
+    if (!list || !sortBy || !order) {
+      return res.status(400).json({
+        message: "The fields 'list', 'sortBy', and 'order' are required.",
+      });
+    }
+    if (!sortField.includes(sortBy)) {
+      return res.status(400).json({
+        message: "Invalid sort type. Only rating or yearOfRelease allowed ",
+      });
+    }
+    if (!allowedLists.includes(list)) {
+      return res.status(400).json({
+        message:
+          "Invalid listType. Valid options are: wishList, watchlist, curatedList.",
+      });
+    }
+
+    let sortColumn;
+    if (sortBy === "rating") {
+      sortColumn = "rating";
+    } else if (sortBy === "yearOfRelease") {
+      sortColumn = "releaseYear";
+    }
+
+    let result;
+
+    switch (list) {
+      case "wishlist": {
+        result = await Wishlist.findAll({ order: [[sortColumn, order]] });
+        break;
+      }
+      case "watchlist": {
+        result = await Watchlist.findAll({ order: [[sortColumn, order]] });
+        break;
+      }
+      case "curatedList": {
+        result = await CuratedListItem.findAll({
+          order: [[sortColumn, order]],
+        });
+        break;
+      }
+      default: {
+        return res.status(400).json({ message: "Invalid listType." });
+      }
+    }
+
+    if (result && result.length > 0) {
+      return res.status(200).json({ movies: result });
+    } else {
+      return res.status(200).json({ movies: [] });
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Error searching movies", error: err.message });
+  }
+};
+
+module.exports = {
+  getMovies,
+  searchMoviesByGenreAndActor,
+  sortByRatingOrYearOfRelease,
+};
