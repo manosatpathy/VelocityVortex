fef680b4da17bb83a042941f233518f3413cf6eb
manosatpathy <manosatpathy@gmail.com>
2024-12-22 23:45:06 +0530

    feat: add route for sortByRatingOrYearOfRelease

--- server/routes/movieRoute.js
+++ server/routes/movieRoute.js
@@ -1,11 +1,13 @@
 const {
   getMovies,
   searchMoviesByGenreAndActor,
+  sortByRatingOrYearOfRelease,
 } = require("../controllers/movieController");
 const express = require("express");
 const router = express.Router();
 
 router.get("/search", getMovies);
 router.get("/searchByGenreAndActor", searchMoviesByGenreAndActor);
+router.get("/sort", sortByRatingOrYearOfRelease);
 
 module.exports = router;
